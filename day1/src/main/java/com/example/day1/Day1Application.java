1e4503bac4028386010030c2c47c9de2dedeb461
Kasipat <kasipat.sho@ttbbank.com>
2024-10-02 11:37:58 +0700

    HelloController

--- day1/src/main/java/com/example/day1/Day1Application.java
+++ day1/src/main/java/com/example/day1/Day1Application.java
@@ -2,12 +2,16 @@ package com.example.day1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Day1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day1Application.class, args);
-	}
-
+  public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication.run(
+      Day1Application.class,
+      args
+    );
+    System.out.println(context.getBeanDefinitionCount());
+  }
 }
